0f69ad356fa111626ab35d8b3e79704e690b47e2
Oleksandr Bondarenko <oleksandr.bondarenko@airslate.com>
2026-03-30 18:22:10 +0300

    fix: add CREATE EXTENSION age to postgres init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,4 +1,4 @@
--- Load Apache AGE extension (already created in 00-create-extension-age.sql)
+CREATE EXTENSION IF NOT EXISTS age;
 CREATE EXTENSION IF NOT EXISTS vector;
 LOAD 'age';
 SET search_path = ag_catalog, "$user", public;
